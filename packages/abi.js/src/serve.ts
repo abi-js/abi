1a89ce679a9e96028114866cc025b0b58b5b69a7
Sigui Kessé Emmanuel <siguici@proton.me>
2024-08-23 16:14:31 +0100

    :label: Fix Node request body retrieval

--- packages/abi.js/src/serve.ts
+++ packages/abi.js/src/serve.ts
@@ -20,25 +20,46 @@ function serve(address: Address, handler: ServeHandler): Address;
 function serve(options: ServeOptions): Address;
 function serve(arg1: any, arg2?: any, arg3?: any): Address {
   const { port, hostname, handler } = toServeOptions(arg1, arg2, arg3);
-  const server = createServer(
-    async (req: IncomingMessage, res: ServerResponse) => {
-      const request = new Request(req.url || '/', {
-        method: req.method || 'GET',
-        headers: new Headers(req.headers as Record<string, string>),
-        body: new Promise<Buffer>((resolve) => {
-          const chunks: Buffer[] = [];
-          req.on('data', (chunk) => chunks.push(chunk));
-          req.on('end', () => resolve(Buffer.concat(chunks)));
-        }),
-      });
 
-      const response = await handler(request);
-      res.writeHead(200, Object.fromEntries(response.headers.entries()));
-      res.end(response.body);
-    },
-  );
+  const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+    const data: Uint8Array[] = [];
+
+    req
+      .on('error', (err) => {
+        throw new Error(err);
+      })
+      .on('data', (chunk) => {
+        data.push(chunk);
+      })
+      .on('end', async () => {
+        const body = Buffer.concat(data);
+        const method = req.method?.toUpperCase() || 'GET';
+        const request = new Request(req.url || '/', {
+          method,
+          headers: Object.entries(req.headers).reduce(
+            (headers, [key, value]) => {
+              if (Array.isArray(value)) {
+                for (const v of value) {
+                  headers.append(key, v);
+                }
+              } else if (value !== undefined) {
+                headers.set(key, value);
+              }
+              return headers;
+            },
+            new Headers(),
+          ),
+          body: method in ['GET', 'HEAD'] ? null : body,
+        });
+
+        const response = await handler(request);
+        res.writeHead(200, Object.fromEntries(response.headers.entries()));
+        res.end(response.body);
+      });
+  });
 
   server.listen(port, hostname);
+
   const address = server.address();
 
   return address && typeof address === 'object'
