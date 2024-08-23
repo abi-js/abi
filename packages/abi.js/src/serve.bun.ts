30b9550e6c1a26f550e9173887e99d2cc1821bed
Sigui Kessé Emmanuel <siguici@proton.me>
2024-08-23 13:56:52 +0100

    :children_crossing: Return the server's listening address

--- packages/abi.js/src/serve.bun.ts
+++ packages/abi.js/src/serve.bun.ts
@@ -1,5 +1,5 @@
 import { getServeOptionsAndHandler } from './helpers';
-import type { ServeHandler, ServeOptions } from './types';
+import type { ListenAddress, ServeHandler, ServeOptions } from './types';
 
 function serve(handler: ServeHandler): void;
 function serve(portOrHost: number | string, handler: ServeHandler): void;
@@ -7,16 +7,19 @@ function serve(options: ServeOptions, handler: ServeHandler): void;
 function serve<T extends ServeOptions | ServeHandler>(
   optionsOrHandler: T,
   handler?: T extends ServeOptions ? ServeHandler : never,
-): void {
+): ListenAddress {
   const [_options, _handler] = getServeOptionsAndHandler(
     optionsOrHandler,
     handler,
   );
-  Bun.serve({
+
+  const server = Bun.serve({
     port: _options.port,
     hostname: _options.hostname,
     fetch: _handler,
   });
+
+  return { port: server.port, hostname: server.hostname };
 }
 
 export { serve };
--- packages/abi.js/src/serve.ts
+++ packages/abi.js/src/serve.ts
@@ -4,7 +4,7 @@ import {
   createServer,
 } from 'node:http';
 import { getServeOptionsAndHandler } from './helpers';
-import type { ServeHandler, ServeOptions } from './types';
+import type { ListenAddress, ServeHandler, ServeOptions } from './types';
 
 function serve(handler: ServeHandler): void;
 function serve(portOrHost: number | string, handler: ServeHandler): void;
@@ -12,26 +12,38 @@ function serve(options: ServeOptions, handler: ServeHandler): void;
 function serve<T extends ServeOptions | ServeHandler>(
   optionsOrHandler: T,
   handler?: T extends ServeOptions ? ServeHandler : never,
-): void {
+): ListenAddress {
   const [_options, _handler] = getServeOptionsAndHandler(
     optionsOrHandler,
     handler,
   );
-  createServer(async (req: IncomingMessage, res: ServerResponse) => {
-    const request = new Request(req.url || '/', {
-      method: req.method || 'GET',
-      headers: new Headers(req.headers as Record<string, string>),
-      body: new Promise<Buffer>((resolve) => {
-        const chunks: Buffer[] = [];
-        req.on('data', (chunk) => chunks.push(chunk));
-        req.on('end', () => resolve(Buffer.concat(chunks)));
-      }),
-    });
+  const server = createServer(
+    async (req: IncomingMessage, res: ServerResponse) => {
+      const request = new Request(req.url || '/', {
+        method: req.method || 'GET',
+        headers: new Headers(req.headers as Record<string, string>),
+        body: new Promise<Buffer>((resolve) => {
+          const chunks: Buffer[] = [];
+          req.on('data', (chunk) => chunks.push(chunk));
+          req.on('end', () => resolve(Buffer.concat(chunks)));
+        }),
+      });
 
-    const response = await handler(request);
-    res.writeHead(200, Object.fromEntries(response.headers.entries()));
-    res.end(response.body);
-  }).listen(_options.port, _options.hostname);
+      const response = await _handler(request);
+      res.writeHead(200, Object.fromEntries(response.headers.entries()));
+      res.end(response.body);
+    },
+  );
+
+  server.listen(_options.port, _options.hostname);
+  const address = server.address();
+
+  return address && typeof address === 'object'
+    ? {
+        port: address.port,
+        hostname: address.address,
+      }
+    : { port: _port, hostname: _hostname };
 }
 
 export { serve };
